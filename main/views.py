1c5e8c8bd4e892dfbbffecef55d097989ad4b9bb
Kellen <117675592+KellenCostaPro@users.noreply.github.com>
2024-08-30 23:34:37 -0300

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def atividade(request):
+    return render(request, 'atividade.html')
---
+++ projeto_todo/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def atividade_sucesso(request):
+    return render(request, 'atividade.html')
